7154f884b244a1dfe0493dede2bbb0ad1520f5d1
Carlos Carrara <carlinhos.alberto308@gmail.com>
2026-04-28 10:48:33 -0300

    First Commit * Add necessary systems

---
+++ CCStore.API/Models/Categoria.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCStore.API.Models
+{
+    public class Categoria
+    {
+        
+    }
+}
